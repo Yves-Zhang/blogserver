fb49eca587437330de0ad78d579990f68edb236d
yvesyfzhang <yvesyfzhang@tencent.com>
2021-08-18 12:04:31 +0800

    submit

--- middleWare/logMiddleWare.go
+++ middleWare/logMiddleWare.go
@@ -7,14 +7,14 @@ import (
 	"github.com/kataras/iris/v12"
 )
 
-func getTime() string{
-	t1:=time.Now().Year()        //年
-	t2:=time.Now().Month()       //月
-	t3:=time.Now().Day()         //日
-	t4:=time.Now().Hour()        //小时
-	t5:=time.Now().Minute()      //分钟
-	t6:=time.Now().Second()      //秒
-	t7:=time.Now().Nanosecond()  //纳秒
+func getTime() string {
+	t1 := time.Now().Year()        //年
+	t2 := time.Now().Month()       //月
+	t3 := time.Now().Day()         //日
+	t4 := time.Now().Hour()        //小时
+	t5 := time.Now().Minute()      //分钟
+	t6 := time.Now().Second()      //秒
+	t7 := time.Now().Nanosecond()  //纳秒
 	return time.Date(t1, t2, t3, t4, t5, t6, t7, time.Local).String()
 }
 
